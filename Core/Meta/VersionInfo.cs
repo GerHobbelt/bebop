034d2d16a3883a2645891c3c34efe00e985332e0
Ger Hobbelt <ger@hobbelt.com>
2022-03-05 10:02:40 +0100

    fix: MSVC2019 does not support NET 6.0 and C# language standard 10.0

--- Core/Meta/VersionInfo.cs
+++ Core/Meta/VersionInfo.cs
@@ -31,6 +31,6 @@
                     /// <summary>
                     /// The human readable version which may contain additional labels.
                     /// </summary>
-                    public const string Informational = "2.3.1-20220305-0843";
+                    public const string Informational = "2.3.1-20220305-0900";
                 }
             }
