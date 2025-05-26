c9f4062730d5cc232629b35d5b75adc886bbc02c
andrew <1297077+andrewmd5@users.noreply.github.com>
2025-05-27 00:40:55 +0900

    chore: use JSON compare

--- Laboratory/Integration/decode.ts
+++ Laboratory/Integration/decode.ts
@@ -1,21 +1,25 @@
-import {Library} from "./schema";
-import {makelib} from "./makelib";
+import { Library } from "./schema";
+import { makelib } from "./makelib";
 import fs from "fs";
 import equal from "deep-equal";
-import {assert} from "console";
+import { assert } from "console";
 import util from "util";
 
 const buffer = fs.readFileSync(process.argv[2]);
 const de = Library.decode(buffer);
 const ex = makelib();
 
-const eq = equal(de, ex, {strict: false});
-if (!eq) {
-    console.log("decoded:")
-    console.log(util.inspect(de, {showHidden: false, depth: null}))
-    console.log()
-    console.log("expected:")
-    console.log(util.inspect(ex, {showHidden: false, depth: null}))
-}
+// JSON.stringify/parse automatically strips functions
+const deJson = JSON.parse(JSON.stringify(de));
+const exJson = JSON.parse(JSON.stringify(ex));
 
+const eq = equal(deJson, exJson, { strict: false });
+
+if (!eq) {
+    console.log("decoded:");
+    console.log(util.inspect(deJson, { showHidden: false, depth: null }));
+    console.log();
+    console.log("expected:");
+    console.log(util.inspect(exJson, { showHidden: false, depth: null }));
+}
 process.exit(eq ? 0 : 1);
